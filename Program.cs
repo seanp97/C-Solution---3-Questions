a9eaee1f4057675f8753a795f8840beb50c7297d
seanp97 <sean.pelser97@gmail.com>
2022-02-16 15:08:36 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+
+namespace ConsoleApplicationProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Questions q = new Questions();
+
+            // Lorem Ipsum
+            int y = q.CounterGeneric<string, char>("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eanean sodales justo et Enim ornare, a congue lacus commodo", 'e');
+            Console.WriteLine(y);
+
+            //Number value count
+            int z = q.CounterGeneric<string, int>("17272838119191929838299111", 1);
+            Console.WriteLine(z);
+
+
+            //Add nums
+            int a = q.AddNums(7, 5);
+            Console.WriteLine(a);
+
+        }
+
+    }
+
+
+}
---
+++ Questions.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationProject
+{
+    public class Questions
+    {
+
+        public int CounterGeneric<T, U>(T a, U b)
+        {
+            int counter = 0;
+
+            //In production would check that a letter is being parsed - try catch
+            if(a.GetType() == typeof(string) && b.GetType() == typeof(char))
+            {
+                string lorStr = a.ToString().ToLower();
+                char[] lorChar = lorStr.ToCharArray();
+
+                char charEquals = (char)(object)b;
+
+                foreach (char c in lorChar)
+                {
+                    if(c == charEquals)
+                    {
+                        counter++;
+                    }
+                }
+            }
+
+            //Number needs to be string as integer is too large
+
+            //In production would check that a letter is being parsed - try catch
+            else if (a.GetType() == typeof(string) && b.GetType() == typeof(int))
+            {
+                string numValue = a.ToString();
+                char[] numValArr = numValue.ToCharArray();
+
+                foreach(char x in numValArr)
+                {
+                    if(x.ToString() == b.ToString())
+                    {
+                        counter++;
+                    }
+                }
+
+            }
+
+            return counter;
+        }
+
+        // Another way is to method overloading but this way there is only one method ^^^^
+
+
+        public int AddNums(int a, int b)
+        {
+            return a + b;
+        }
+
+    }
+}
